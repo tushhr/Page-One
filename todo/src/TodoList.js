21f8499e34352a410255f3956f2ec6653a14a0ac
tushhr <tushar.maheshwari@scaler.com>
2023-01-13 13:15:59 +0530

    User can change status of task

--- todo/src/TodoList.js
+++ todo/src/TodoList.js
@@ -1,21 +1,26 @@
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { TodoListContext } from "./context"
 
 export default function TodoList() {
 	const {todoList, setTodoList} = useContext(TodoListContext);
-	
-	const delelteTodo = (todo) => {
-		console.log("Han delete kr do!!", todo)
 
+	const delelteTodo = (todo) => {
 		setTodoList(todoList.filter((tmp) => {return tmp !== todo}))
+	}
+
+	const changeStatusTodo = (todo) =>{
+		setTodoList(todoList.map(e => 
+				e == todo ? {...todo, status: !todo.status} : e
+			))
 	}
 
 	return (
 		<div className='todos-list'>
         	{todoList.map((todo, index) => ( 
-          		<div> 
+          		<div>
+					<input type="checkbox" checked={todo.status} onChange={() => changeStatusTodo(todo)}></input>
            			<p key={index} className = {todo.status ? "hidden" : "show"}>{todo.task} </p> 
-					<button onClick={() => delelteTodo(todo) }>Delete</button>
+					<button onClick={() => delelteTodo(todo)}>Delete</button>
 				</div> 
 			))}
     	</div>
